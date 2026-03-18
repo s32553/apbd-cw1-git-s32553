01163f0095d449bc7aa9125add3748158f9f8885
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-03-18 20:13:44 +0100

    Dodanie wywołania CalculateAverage i wyświetlenia średniej

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ class Program
     {
         Console.WriteLine("Podaj liczby oddzielone spacją:");
         var input = Console.ReadLine();
-        var numbers = Array.ConvertAll(input.Split(' '), int.Parse);
+        try
+        {
+            var numbers = Array.ConvertAll(input.Split(' '), int.Parse);
+            Console.WriteLine("Wczytane liczby: " + string.Join(", ", numbers));
 
-        Console.WriteLine("Wczytane liczby: " + string.Join(", ", numbers));
+            double average = APBD_CW1.StatisticsHelper.CalculateAverage(numbers);
+            Console.WriteLine("Średnia wczytanych liczb: " + average);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Błąd: Podano nieprawidłowe dane.");
+        }
     }
 }
