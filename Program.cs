6e11a12a514a17155983410859733b0a65959619
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-03-18 20:09:42 +0100

    Dodanie wczytania z konsoli

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczby oddzielone spacją:");
+        var input = Console.ReadLine();
+        var numbers = Array.ConvertAll(input.Split(' '), int.Parse);
+
+        Console.WriteLine("Wczytane liczby: " + string.Join(", ", numbers));
+    }
+}
