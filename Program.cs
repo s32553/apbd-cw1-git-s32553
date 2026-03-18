beaef35e79b65e040e28fdbd59013ef90593ab33
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-03-18 20:33:02 +0100

    Zmiana komunikatu w main

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Witaj! Podaj liczby oddzielone spacją:");
+        Console.WriteLine("Witaj! Podaj liczby oddzielene przecinkami:");
         var input = Console.ReadLine();
         try
         {
