c56cb3b4ae3a1c0ad3a26c1cf128ba55828c2038
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-03-18 20:34:59 +0100

    Zmiana komunikatu w feature-conflict2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Witaj! Podaj liczby oddzielene przecinkami:");
+        Console.WriteLine("Witaj! Podaj liczby oddzielene przecinkami (feature conflict):");
         var input = Console.ReadLine();
         try
         {
