17857ce81b0523827af5f48a89c82263d7877afc
Piotr Zaremba-Czereyski <s32553@pjwstk.edu.pl>
2026-03-18 20:29:00 +0100

    Dodanie obsługi wyjątków i komentarzy w CalculateMin

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -4,37 +4,29 @@ namespace APBD_CW1
 {
     public static class StatisticsHelper
     {
-        public static double CalculateAverage(int[] values)
-        {
-            if (values.Length == 0) return 0;
-            double sum = 0;
-            foreach (var v in values) sum += v;
-            return sum / values.Length;
-        }
-
-        public static double CalculateAverage(int[] values)
-        {
-            if (values.Length == 0) return 0;
-            double sum = 0;
-            foreach (var v in values) sum += v;
-            return sum / values.Length;
-        }
-        public static int CalculateMax(int[] values)
-        {
-            if (values.Length == 0) throw new ArgumentException("Brak danych");
-            int max = values[0];
-            foreach (var v in values)
-            if (v > max) max = v;
-            return max;
-        }
+        // Metoda oblicza minimalną wartość w tablicy liczb całkowitych
+        // Zgłasza wyjątek, jeśli tablica jest pusta
         public static int CalculateMin(int[] values)
         {
-            if (values.Length == 0) throw new ArgumentException("Brak danych");
+            if (values.Length == 0)
+            {
+                // Wyjątek informujący o braku danych
+                throw new ArgumentException("Brak danych do obliczenia min");
+            }
+
+            // Inicjalizacja minimalnej wartości pierwszym elementem
             int min = values[0];
+
+            // Pętla sprawdzająca każdy element tablicy
             foreach (var v in values)
-            if (v < min) min = v;
-            return min;
+            {
+                if (v < min)
+                {
+                    min = v; // Aktualizacja minimalnej wartości
+                }
+            }
+
+            return min; // Zwracamy najmniejszą wartość
         }
     }
-
-}ź
+}
